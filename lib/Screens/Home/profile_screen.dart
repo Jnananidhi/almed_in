651b47e0e1f7adc40fb4f1a89b1d898c50b36877
Jnananidhi <mjnananidhi35@gmail.com>
2023-10-25 21:54:29 +0530

    profile screen UI Done v2

--- lib/Screens/Home/profile_screen.dart
+++ lib/Screens/Home/profile_screen.dart
@@ -1,3 +1,8 @@
+import 'package:almed_in/Screens/Home/faq_screen.dart';
+import 'package:almed_in/Screens/Home/home_screen.dart';
+import 'package:almed_in/Screens/Home/widgets/bottomnav.dart';
+import 'package:almed_in/Screens/Home/widgets/menu.dart';
+import 'package:almed_in/constants.dart';
 import 'package:flutter/material.dart';
 
 
@@ -5,63 +10,256 @@ import 'package:flutter/material.dart';
 class UserProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body:UserProfilePage()
-    );
+   return Scaffold(
+        backgroundColor: kWhiteColor,
+        //scrollable widget
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              const DrawerHeader(
+                child: Center(
+                  child: Image(image: AssetImage('logo.png')),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    MenuItems(
+                      isActive: false,
+                      title: 'Home',
+                      press: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const HomeScreen()),
+                        );
+                      },
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    MenuItems(
+                      title: 'About Us',
+                      press: () {},
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    MenuItems(
+                      title: 'Why Almed',
+                      press: () {},
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    MenuItems(
+
+                      title: 'FAQ',
+                      press: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const FaqScreen()),
+                        );
+                      },
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    MenuItems(
+                      isActive: true,
+                      title: 'Contact Us',
+                      press: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const HomeScreen()),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        body: Column(
+            children: <Widget>[
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      //now we create menu and header
+                      const Navigation(),
+                      UserProfilePage(),
+                      const BottomNav()
+                      //now we will make our site responsive
+                    ],
+                  ),
+                ),
+              )]));
   }
 }
+
 
 class UserProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('User Profile'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          SizedBox(height: 20),
-          CircleAvatar(
-            radius: 60,
-            backgroundImage: AssetImage('assets/profile_picture.jpg'), // Replace with the path to the user's profile picture
-          ),
-          SizedBox(height: 10),
-          Text(
-            'John Doe', // Replace with the user's name
-            style: TextStyle(fontSize: 24),
-          ),
-          SizedBox(height: 10),
-          Text(
-            'john.doe@example.com', // Replace with the user's email
-            style: TextStyle(fontSize: 18),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              // Implement the functionality to edit user information
-              // This button can open a form for editing user details
-            },
-            child: Text('Edit Profile'),
-          ),
-          SizedBox(height: 20),
-          Text(
-            'Order History', // You can customize the title
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          // Display a list of user's order history
-          // You can use a ListView.builder or any other widget for this purpose
-          Expanded(
-            child: ListView(
+    return  Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding:  EdgeInsets.only(left: 0,top:30,right: 30,bottom:10),
+          child: Column(
+
               children: <Widget>[
-                OrderHistoryItem(orderNumber: 'Order #12345', date: '2023-01-15', total: '\$99.99'),
-                OrderHistoryItem(orderNumber: 'Order #67890', date: '2023-03-20', total: '\$49.99'),
-                // Add more order history items here
+                SizedBox(height: 20),
+                CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage('products/users/profile_image.jpg'), // Replace with the path to the user's profile picture
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'John Doe', // Replace with the user's name
+                  style: TextStyle(fontSize: 24),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'john.doe@example.com', // Replace with the user's email
+                  style: TextStyle(fontSize: 18),
+                ),
+                SizedBox(height: 20),
+                Card(
+                  // Set card margin
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0), // Set the curved border radius
+                  ),
+                  elevation: 10,
+                  child: Container(
+                    padding: EdgeInsets.all(20.0),
+                    child:  Column(
+                        children: <Widget>[
+                          TextButton(
+                            onPressed:  () {
+
+                            },
+                            child: Text(
+                              'My Orders',textAlign: TextAlign.right,
+                              style: TextStyle(
+                                fontFamily: "DMSans Regular",
+                                color: Colors.black,
+                                decoration: TextDecoration.underline,
+                              ),
+                            ),
+                          ),
+                          Divider(
+                            thickness: 1,
+                            indent : 0,
+                            endIndent : 10,
+                          ),
+                          TextButton(
+                            onPressed:  () {
+
+                            },
+                            child: Text(
+                              'Logout',textAlign: TextAlign.right,
+                              style: TextStyle(
+                                fontFamily: "DMSans Regular",
+                                color: Colors.black,
+                                decoration: TextDecoration.underline,
+                              ),
+                            ),
+                          ),                    ],
+                    ),
+                  ),
+                ),
+                    ],
+            ),
+        ),
+        Card(
+           // Set card margin
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0), // Set the curved border radius
+          ),
+          elevation: 10,
+          child: Container(
+            padding: EdgeInsets.all(20.0),
+            width: kMaxWidth/1.5,
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text("Personal Information",textAlign: TextAlign.left,style: TextStyle(color: kPrimaryColor,fontFamily: 'DMSans Regular')),
+                SizedBox(height: 10,),
+                Card(
+                  elevation: 20,
+                  color: lightColor,
+                  child: Padding(
+                    padding: EdgeInsets.all(30.0),
+                  child: Column(
+
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Phone",textAlign: TextAlign.start,style: TextStyle(color: kPrimaryColor,fontFamily: 'DMSans Regular')),
+                      Text("+91 8088888688",textAlign: TextAlign.start,style: TextStyle(color: Colors.black,fontFamily: 'DMSans Regular')),
+                      Divider(
+                        thickness: 1,
+                        indent : 0,
+                        endIndent : 10,
+                      ),
+                      Text("Email",textAlign: TextAlign.start,style: TextStyle(color: kPrimaryColor,fontFamily: 'DMSans Regular')),
+                      Text("yourmail@gmail.com",textAlign: TextAlign.start,style: TextStyle(color: Colors.black,fontFamily: 'DMSans Regular')),
+                      Divider(
+                        thickness: 1,
+                        indent : 0,
+                        endIndent : 10,
+                      ),
+                      Text("Shipping Address",textAlign: TextAlign.start,style: TextStyle(color: kPrimaryColor,fontFamily: 'DMSans Regular')),
+                      Text("your full address",textAlign: TextAlign.start,style: TextStyle(color: Colors.black,fontFamily: 'DMSans Regular')),
+
+                    ],
+                  ),
+                  ),
+                ),
+                Text("Payments Information",textAlign: TextAlign.left,style: TextStyle(color: kPrimaryColor,fontFamily: 'DMSans Regular')),
+                SizedBox(height: 10,),
+                Card(
+                  elevation: 20,
+                  color: lightColor,
+                  child: Padding(
+                    padding: EdgeInsets.all(30.0),
+                    child: Column(
+
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Saved Cards",textAlign: TextAlign.start,style: TextStyle(color: kPrimaryColor,fontFamily: 'DMSans Regular')),
+                        Text("+91 8088888688",textAlign: TextAlign.start,style: TextStyle(color: Colors.black,fontFamily: 'DMSans Regular')),
+                        Divider(
+                          thickness: 1,
+                          indent : 0,
+                          endIndent : 10,
+                        ),
+                        Text("Saved UPI",textAlign: TextAlign.start,style: TextStyle(color: kPrimaryColor,fontFamily: 'DMSans Regular')),
+                        Text("yourmail@gmail.com",textAlign: TextAlign.start,style: TextStyle(color: Colors.black,fontFamily: 'DMSans Regular')),
+                        Divider(
+                          thickness: 1,
+                          indent : 0,
+                          endIndent : 10,
+                        ),
+                        Text("Shipping Address",textAlign: TextAlign.start,style: TextStyle(color: kPrimaryColor,fontFamily: 'DMSans Regular')),
+                        Text("your full address",textAlign: TextAlign.start,style: TextStyle(color: Colors.black,fontFamily: 'DMSans Regular')),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
